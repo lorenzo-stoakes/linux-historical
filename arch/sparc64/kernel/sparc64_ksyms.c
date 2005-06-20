bc4a598be8faad234847a448516708ee9c1b427f
David S. Miller <davem@davemloft.net>
2005-06-19 18:28:11 -0700

    [SPARC64]: Fix conflicting __bzero_noasi() prototypes.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/sparc64_ksyms.c
+++ arch/sparc64/kernel/sparc64_ksyms.c
@@ -63,7 +63,6 @@ extern unsigned prom_cpu_nodes[64];
 extern void die_if_kernel(char *str, struct pt_regs *regs);
 void _sigpause_common (unsigned int set, struct pt_regs *);
 extern void *__bzero(void *, size_t);
-extern void *__bzero_noasi(void *, size_t);
 extern void *__memscan_zero(void *, size_t);
 extern void *__memscan_generic(void *, int, size_t);
 extern int __memcmp(const void *, const void *, __kernel_size_t);
