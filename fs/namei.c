f41e0ce901260d3d1ae5bd8bae34266891b4a65d
Marcelo Tosatti <marcelo@dmt.cnet>
2006-05-12 15:02:33 -0300

    Vadim Egorov: ext3 link/unlink race
    I found this issue with 2.4.27 kernel but I believe that other versions
    are affected too.
    
    The problem happens when link and unlink are invoked simultaneously on
    the same inode on ext3 filesystem. In this case ext3_unlink may
    decrement i_nlink to 0 and put this inode into the in-memory orphan
    list, while ext3_link will increment i_nlink back to 1 having the inode
    in the orphan list.
    
    Thus the system ends up having an inode with i_nlink == 1 in the orphan
    list.
    
    When this inode gets unused later it the memory might get released to
    the free pool and then be used for some other purpose, most likely some
    other inode.
    
    From this point on any operation on the orphan list may result in
    modification of the list_head that could alredy be used to store some
    other date.

--- fs/namei.c
+++ fs/namei.c
@@ -1479,7 +1479,7 @@ int vfs_unlink(struct inode *dir, struct dentry *dentry)
 {
 	int error;
 
-	down(&dir->i_zombie);
+	double_down(&dir->i_zombie, &dentry->d_inode->i_zombie);
 	error = may_delete(dir, dentry, 0);
 	if (!error) {
 		error = -EPERM;
@@ -1491,14 +1491,14 @@ int vfs_unlink(struct inode *dir, struct dentry *dentry)
 				lock_kernel();
 				error = dir->i_op->unlink(dir, dentry);
 				unlock_kernel();
-				if (!error)
-					d_delete(dentry);
 			}
 		}
 	}
-	up(&dir->i_zombie);
-	if (!error)
+	double_up(&dir->i_zombie, &dentry->d_inode->i_zombie);
+	if (!error) {
+		d_delete(dentry);
 		inode_dir_notify(dir, DN_DELETE);
+	}
 	return error;
 }
 
@@ -1607,18 +1607,19 @@ int vfs_link(struct dentry *old_dentry, struct inode *dir, struct dentry *new_de
 	struct inode *inode;
 	int error;
 
-	down(&dir->i_zombie);
 	error = -ENOENT;
 	inode = old_dentry->d_inode;
 	if (!inode)
-		goto exit_lock;
-
-	error = may_create(dir, new_dentry);
-	if (error)
-		goto exit_lock;
+		goto exit;
 
 	error = -EXDEV;
 	if (dir->i_dev != inode->i_dev)
+		goto exit;
+
+	double_down(&dir->i_zombie, &old_dentry->d_inode->i_zombie);
+
+	error = may_create(dir, new_dentry);
+	if (error)
 		goto exit_lock;
 
 	/*
@@ -1636,9 +1637,10 @@ int vfs_link(struct dentry *old_dentry, struct inode *dir, struct dentry *new_de
 	unlock_kernel();
 
 exit_lock:
-	up(&dir->i_zombie);
+	double_up(&dir->i_zombie, &old_dentry->d_inode->i_zombie);
 	if (!error)
 		inode_dir_notify(dir, DN_CREATE);
+exit:
 	return error;
 }
 
