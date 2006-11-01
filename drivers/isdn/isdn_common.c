c6f1472052f9aeb267cb770162802e6c57f7d63f
Jeff Garzik <jeff@garzik.org>
2006-10-31 21:34:29 -0800

    [PATCH] ISDN: fix drivers, by handling errors thrown by ->readstat()
    (backport from 2.6-stable)
    
    This is a particularly ugly on-failure bug, possibly security, since the
    lack of error handling here is covering up another class of bug: failure to
    handle copy_to_user() return values.
    
    The I4L API function ->readstat() returns an integer, and by looking at
    several existing driver implementations, it is clear that a negative return
    value was meant to indicate an error.
    
    Given that several drivers already return a negative value indicating an
    errno-style error, the current code would blindly accept that [negative]
    value as a valid amount of bytes read.  Obvious damage ensues.
    
    Correcting ->readstat() handling to properly notice errors fixes the
    existing code to work correctly on error, and enables future patches to
    more easily indicate errors during operation.
    
    Signed-off-by: Jeff Garzik <jeff@garzik.org>
    Cc: Karsten Keil <kkeil@suse.de>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    
    For 2.4 backport :
    Acked-by: Karsten Keil <kkeil@suse.de>

--- drivers/isdn/isdn_common.c
+++ drivers/isdn/isdn_common.c
@@ -1058,6 +1058,10 @@ isdn_read(struct file *file, char *buf, size_t count, loff_t * off)
 			len = dev->drv[drvidx]->interface->
 				readstat(buf, count, 1, drvidx,
 					 isdn_minor2chan(minor));
+			if (len < 0) {
+				retval = len;
+				goto out;
+			}
 		} else {
 			len = 0;
 		}
