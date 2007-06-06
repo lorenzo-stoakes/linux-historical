c2afbe1daf23e6501914d1ea294660a47ed6172e
Willy Tarreau <w@1wt.eu>
2007-06-06 08:29:49 +0200

    [PATCH] lvm: do not update extent count if snapshot allocation fails
    Fix from RHEL3 U8, explained by Heinz Mauelshagen :
    
    That change makes sure that the extent count *only* gets updated in
    case the allocation of the snapshot succeeds. Like you said: proper
    error path handling.

--- drivers/md/lvm.c
+++ drivers/md/lvm.c
@@ -2187,10 +2187,6 @@ static int lvm_do_lv_create(int minor, char *lv_name, lv_t * lv)
 				lv_ptr->lv_stripesize =
 				    lv_ptr->lv_snapshot_org->lv_stripesize;
 
-				/* Update the VG PE(s) used by snapshot reserve space. */
-				vg_ptr->pe_allocated +=
-				    lv_ptr->lv_allocated_snapshot_le;
-
 				if ((ret =
 				     lvm_snapshot_alloc(lv_ptr)) != 0) {
 					vfree(lv_ptr->lv_block_exception);
@@ -2198,6 +2194,10 @@ static int lvm_do_lv_create(int minor, char *lv_name, lv_t * lv)
 					vg_ptr->lv[l] = NULL;
 					return ret;
 				}
+				/* Update the VG PE(s) used by snapshot reserve space. */
+				vg_ptr->pe_allocated +=
+				    lv_ptr->lv_allocated_snapshot_le;
+
 				for (e = 0; e < lv_ptr->lv_remap_ptr; e++)
 					lvm_hash_link(lv_ptr->
 						      lv_block_exception +
