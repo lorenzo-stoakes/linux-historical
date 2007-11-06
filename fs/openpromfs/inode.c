6ab2cfa4f0a04c11932af701b5437879dd14d8bb
dann frazier <dannf@dannf.org>
2007-11-06 15:37:56 -0700

    [PATCH 3/4] [OpenPROM] Prevent overflow of sprintf buffer
    This patch fixes a few potential overflows, originally submitted to 2.5 by
    Dave Miller:
     http://linux.bkbits.net:8080/linux-2.6/?PAGE=cset&REV=3d69d753xoJv6rAeuQzdAcJK6Njncg
     http://linux.bkbits.net:8080/linux-2.6/?PAGE=cset&REV=3d6aabcc3jBCcQB6wlZ7s3G9WGPYsg
    
    Signed-off-by: dann frazier <dannf@hp.com>

--- fs/openpromfs/inode.c
+++ fs/openpromfs/inode.c
@@ -285,24 +285,27 @@ static ssize_t property_read(struct file *filp, char *buf,
 		k += count;
 
 	} else if (op->flag & OPP_HEXSTRING) {
-		char buffer[2];
+		char buffer[3];
 
 		if ((k < i - 1) && (k & 1)) {
-			sprintf (buffer, "%02x", *(op->value + (k >> 1)));
+			sprintf (buffer, "%02x",
+				 (unsigned char) *(op->value + (k >> 1)) & 0xff);
 			if (put_user(buffer[1], &buf[k++ - pos]))
 				return -EFAULT;
 			count--;
 		}
 
 		for (; (count > 1) && (k < i - 1); k += 2) {
-			sprintf (buffer, "%02x", *(op->value + (k >> 1)));
+			sprintf (buffer, "%02x",
+				 (unsigned char) *(op->value + (k >> 1)) & 0xff);
 			if (copy_to_user (buf + k - pos, buffer, 2))
 				return -EFAULT;
 			count -= 2;
 		}
 
 		if (count && (k < i - 1)) {
-			sprintf (buffer, "%02x", *(op->value + (k >> 1)));
+			sprintf (buffer, "%02x",
+				 (unsigned char) *(op->value + (k >> 1)) & 0xff);
 			if (put_user(buffer[0], &buf[k++ - pos]))
 				return -EFAULT;
 			count--;
