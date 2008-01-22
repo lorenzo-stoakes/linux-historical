e942af21b7fda832e02e10941b5696b7b3075025
dann frazier <dannf@debian.org>
2008-01-21 17:14:49 -0700

    avoid semi-infinite loop when mounting bad ext2
    This is a 2.4 backport of a linux-2.6 change by Andries Brouwer
    (old-2.6-bkcvs commit c279c5343b1796bf1db4c0b4af2c99479a6575fe)
    
    Commit log from 2.6 follows.
    
      The routine ext2_readdir() will, when reading a directory page
      returns an error, try the next page, without reporting the
      error to user space. That is bad, and the patch below changes that.
    
      In my case the filesystem was damaged, and ext2_readdir wanted
      to read 60000+ pages and wrote as many error messages to syslog
      ("attempt to access beyond end"), not what one wants.
    
      [no doubt a similar patch is appropriate for ext3]
    
    Signed-off-by: dann frazier <dannf@hp.com>

--- fs/ext2/dir.c
+++ fs/ext2/dir.c
@@ -246,6 +246,7 @@ ext2_readdir (struct file * filp, void * dirent, filldir_t filldir)
 	unsigned chunk_mask = ~(ext2_chunk_size(inode)-1);
 	unsigned char *types = NULL;
 	int need_revalidate = (filp->f_version != inode->i_version);
+	int ret = 0;
 
 	if (pos > inode->i_size - EXT2_DIR_REC_LEN(1))
 		goto done;
@@ -258,8 +259,10 @@ ext2_readdir (struct file * filp, void * dirent, filldir_t filldir)
 		ext2_dirent *de;
 		struct page *page = ext2_get_page(inode, n);
 
-		if (IS_ERR(page))
-			continue;
+		if (IS_ERR(page)) {
+			ret = -EIO;
+			goto done;
+		}
 		kaddr = page_address(page);
 		if (need_revalidate) {
 			offset = ext2_validate_entry(kaddr, offset, chunk_mask);
@@ -291,7 +294,7 @@ done:
 	filp->f_pos = (n << PAGE_CACHE_SHIFT) | offset;
 	filp->f_version = inode->i_version;
 	UPDATE_ATIME(inode);
-	return 0;
+	return ret;
 }
 
 /*
